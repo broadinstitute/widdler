9348d8fe05932d5d26175bd9e576899a10de8b62
Amr Abouelleil <amr@dry-kidz.com>
2017-07-14 15:10:50 -0400

    started work on validation.

--- tests/test_cromwell.py
+++ tests/test_cromwell.py
@@ -13,7 +13,7 @@ class CromwellUnitTests(unittest.TestCase):
         resources = c.resource_dir
         self.logger = logging.getLogger('test_cromwell')
         hdlr = logging.FileHandler(os.path.join(c.log_dir, 'test_cromwell.log'))
-        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         hdlr.setFormatter(formatter)
         self.logger.addHandler(hdlr)
         self.logger.setLevel(logging.INFO)
