bb7febadc2b25c760b8923df3f04d6c9cf2d03ea
Amr Abouelleil <amr@dry-kidz.com>
2017-07-18 16:24:07 -0400

    Validation progress

--- src/Validator.py
+++ src/Validator.py
@@ -3,17 +3,117 @@ import logging
 import json
 import config as c
 import os
-
+import subprocess
+module_logger = logging.getLogger('widdler.Validator')
+import csv
 
 class Validator:
     """
-    Module to validate WDL and JSON inputs.
+    Module to validate JSON inputs.
     """
-    def __init__(self):
+    def __init__(self, wdl, json):
+        self.wdl = wdl
+        self.json = json
         self.wdl_tool = os.path.join(c.resource_dir, 'wdltool-0.10.jar')
+        self.logger = logging.getLogger('widdler.validator.Validator')
 
-    def get_json(self, json):
-        pass
+    def get_json(self):
+        """
+        Get a dict representation of the json file.
+        :return: dict
+        """
+        fh = open(self.json)
+        json_data = json.load(fh)
+        fh.close()
+        return json_data
 
-    def validate_json(self, json):
-        pass
+    def get_wdl_args(self):
+        """
+        Uses wdl-tool to get the expected arguments from the WDL file.
+        :return: Returns a dictionary of wdl arguments as keys and expected type as as value.
+        """
+        cmd = ['java', '-jar', self.wdl_tool, 'inputs', self.wdl]
+        run = subprocess.check_output(cmd).decode("utf-8")
+        return json.loads(run)
+
+    def validate_json(self):
+        errors = list()
+        jdict = self.get_json()
+        wdict = self.get_wdl_args()
+        # for every key/value pair in jdict
+        # first make sure the key is in wdict. If it isn't, that's an error.
+        # return a list of any errors uncovered.
+        for param, val in jdict.items():
+            if self.validate_param(param, wdict):
+                # param is valid
+                print(param, wdict[param])
+            else:
+                # param doesn't exist, add it to errors.
+                errors.append('{} is not a valid WDL parameter.'.format(param))
+
+    def validate_param(self, param, wdict):
+        """
+        Returns true if the param exists in WDL, false if not.
+        :param param: the param to evaluate
+        :param wdict: dictionary of wdl args
+        :return: Boolean
+        """
+        if param in wdict:
+            return True
+        else:
+            return False
+
+    def validate_string(self, i):
+        """
+        Returns true if object is string, false if not.
+        :param i: input object
+        :return: Boolean
+        """
+        return isinstance(i, str)
+
+    def validate_file(self, f):
+        return os.path.exists(f)
+
+    def validate_samples_file(self, samples_file):
+        """
+        Validates a TSV sample file used in WDL inputs. Assumes that last column of each row contains an absolute
+        path to a file.
+        :param samples_file: a tab-delimitted file with the last column of each row containing a file path.
+        :return: A list of errors. If list is empty, there were no errors.
+        """
+        errors = []
+        try:
+            with open(samples_file) as fh:
+                reader = csv.reader(fh, delimiter='\t')
+                for row in reader:
+                    if not self.validate_file(row[-1]):
+                        errors.append('File path {} does not exist.'.format(row[-1]))
+            fh.close()
+        except FileNotFoundError as e:
+            self.logger.error(e)
+            errors.append(e)
+        return errors
+
+    def validate_boolean(self, i):
+        """
+        Returns true if object is a boolean, false if not.
+        :param i: input object
+        :return: Boolean
+        """
+        return isinstance(i, bool)
+
+    def validate_int(self, i):
+        """
+        Returns true if object is an int, false if not.
+        :param i: input object
+        :return: Boolean
+        """
+        return isinstance(i, int)
+
+    def validate_float(self, i):
+        """
+        Returns true if object is a float, false if not.
+        :param i: input object
+        :return: Boolean
+        """
+        return isinstance(i, float)
--- tests/test_validate.py
+++ tests/test_validate.py
@@ -7,6 +7,9 @@ from Validator import Validator
 
 
 class ValidatorUnitTests(unittest.TestCase):
+    """
+    This test class tests good inputs only. A separate one for bad inputs is below.
+    """
     @classmethod
     def setUpClass(self):
         resources = c.resource_dir
@@ -16,12 +19,92 @@ class ValidatorUnitTests(unittest.TestCase):
         hdlr.setFormatter(formatter)
         self.logger.addHandler(hdlr)
         self.logger.setLevel(logging.INFO)
+        self.wdl = os.path.join(resources, 'test.wdl')
         self.json = os.path.join(resources, 'test.json')
-        self.validator = Validator()
+        self.validator = Validator(wdl=self.wdl, json=self.json)
+        self.logger.info('Starting Validator tests...')
+        self.wdl_args = self.validator.get_wdl_args()
+        self.json_args = self.validator.get_json()
+
+    def test_get_wdl_args(self):
+        self.assertIsInstance(self.wdl_args, dict)
+
+    def test_get_json(self):
+        self.logger.info("Testing get_json...")
+        self.assertIsInstance(self.json_args, dict)
+
+    def test_validate_param_json(self):
+        self.logger.info("Testing validate_param_json...")
+        for k, v in self.json_args.items():
+            self.assertIs(self.validator.validate_param(k, self.wdl_args), True)
+
+    def test_validate_string(self):
+        self.logger.info("Testing validate_string...")
+        self.assertIs(self.validator.validate_string('foo'), True)
+        self.assertIs(self.validator.validate_string(0), False)
+        self.assertIs(self.validator.validate_string(1.0), False)
+        self.assertIs(self.validator.validate_string(True), False)
+
+    def test_validate_boolean(self):
+        self.logger.info("Testing validate_boolean...")
+        self.assertIs(self.validator.validate_boolean('foo'), False)
+        self.assertIs(self.validator.validate_boolean(0), False)
+        self.assertIs(self.validator.validate_boolean(1.0), False)
+        self.assertIs(self.validator.validate_boolean(True), True)
+
+    def test_validate_int(self):
+        self.logger.info("Testing validate_int...")
+        self.assertIs(self.validator.validate_int('foo'), False)
+        self.assertIs(self.validator.validate_int(0), True)
+        self.assertIs(self.validator.validate_int(1.0), False)
+        self.assertIs(self.validator.validate_int(True), True)
+
+    def test_validate_float(self):
+        self.logger.info("Testing validate_float...")
+        self.assertIs(self.validator.validate_float('foo'), False)
+        self.assertIs(self.validator.validate_float(0), False)
+        self.assertIs(self.validator.validate_float(1.0), True)
+        self.assertIs(self.validator.validate_float(True), False)
+
+    def test_validate_file(self):
+        self.assertIs(self.validator.validate_file(self.wdl), True)
+        self.assertIs(self.validator.validate_file('notexists.txt'), False)
+
+    def test_validate_samples_file(self):
+        samples_file = os.path.join(c.resource_dir, 'test.tsv')
+        result = self.validator.validate_samples_file(samples_file)
+        self.assertEqual(6, len(result))
+
+    @classmethod
+    def tearDownClass(self):
+        self.logger.info("Test done!")
 
 
-    def test_validate_json(self):
-        pass
+class ValidatorUnitTestsBad(unittest.TestCase):
+    """
+    This test class tests good inputs only. A separate one for bad inputs is below.
+    """
+    @classmethod
+    def setUpClass(self):
+        resources = c.resource_dir
+        self.logger = logging.getLogger('test_validator')
+        hdlr = logging.FileHandler(os.path.join(c.log_dir, 'test_validator.log'))
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        hdlr.setFormatter(formatter)
+        self.logger.addHandler(hdlr)
+        self.logger.setLevel(logging.INFO)
+        self.wdl = os.path.join(resources, 'test.wdl')
+        self.json = os.path.join(resources, 'bad.json')
+        self.validator = Validator(wdl=self.wdl, json=self.json)
+        self.logger.info('Starting Validator tests...')
+        self.wdl_args = self.validator.get_wdl_args()
+        self.json_args = self.validator.get_json()
+
+    def test_validate_param_json(self):
+        self.logger.info("Testing validate_param_json...")
+        ref_dict = {"gatk.samples_file": "/cil/shed/sandboxes/amr/dev/gatk_pipeline/data/pfal_5.tsv"}
+        for k, v in self.json_args.items():
+            self.assertIs(self.validator.validate_param(k, ref_dict), False)
 
     @classmethod
     def tearDownClass(self):
