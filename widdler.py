64f6544f5eddd2217dc4a6cc37417f6107914eff
amr <415MainStreet>
2017-07-24 10:08:52 -0400

    unsuppressed help for validation.

--- widdler.py
+++ widdler.py
@@ -53,7 +53,7 @@ run = sub.add_parser(name='run',
 run.add_argument('wdl', action='store', help='Path to the WDL to be executed.')
 run.add_argument('json', action='store', help='Path the json inputs file.')
 run.add_argument('-v', '--validate', action='store_true', default=False,
-                 help=argparse.SUPPRESS)
+                 help='Validate WDL inputs in json file.')
 run.add_argument('-S', '--server', action='store', required=True, type=str, choices=c.servers,
                  help='Choose a cromwell server from {}'.format(c.servers))
 run.set_defaults(func=call_run)
