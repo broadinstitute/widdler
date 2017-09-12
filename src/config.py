2be49867886958c197ba22dd33ab9c3e5a907c47
paulcao <paulcao@broadinstitute.org>
2017-09-12 17:42:29 -0400

    Check in gscid-cromwell for Widdler.

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 import os
 
-servers = ['ale', 'btl-cromwell', 'localhost']
+servers = ['ale', 'btl-cromwell', 'localhost', 'gscid-cromwell']
 resource_dir = os.path.abspath(os.path.dirname(__file__)).replace('src', 'resources')
 log_dir = os.path.abspath(os.path.dirname(__file__)).replace('src', 'logs')
