5317bdd851d09426497d0d1042236a9f5aef9d0a
Amr Abouelleil <amr@dry-kidz.com>
2017-09-14 13:36:56 -0400

    -implemented new user-level monitoring code.

--- src/config.py
+++ src/config.py
@@ -3,3 +3,4 @@ import os
 servers = ['ale', 'btl-cromwell', 'localhost', 'gscid-cromwell']
 resource_dir = os.path.abspath(os.path.dirname(__file__)).replace('src', 'resources')
 log_dir = os.path.abspath(os.path.dirname(__file__)).replace('src', 'logs')
+run_states = ['Running', 'Submitted', 'QueuedInCromwell']
