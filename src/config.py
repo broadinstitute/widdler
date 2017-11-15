681aaad7069ad127d6106d2b3a25ca28d2dfbc3d
Amr Abouelleil <amr@dry-kidz.com>
2017-11-15 11:14:23 -0500

    -updated unit tests -fixed issue where some config items got deleted.

--- src/config.py
+++ src/config.py
@@ -1,6 +1,13 @@
 import os
+import sys
 
 servers = ['ale1', 'btl-cromwell', 'localhost', 'gscid-cromwell']
 resource_dir = os.path.abspath(os.path.dirname(__file__)).replace('src', 'resources')
-log_dir = os.path.abspath(os.path.dirname(__file__)).replace('src', 'logs')
 run_states = ['Running', 'Submitted', 'QueuedInCromwell']
+terminal_states = ['Failed', 'Aborted', 'Succeeded']
+if sys.platform == 'win32':
+    log_dir = os.path.abspath(os.path.dirname(__file__)).replace('src', 'logs')
+else:
+    log_dir = "/cil/shed/apps/internal/widdler/logs/"
+
+
