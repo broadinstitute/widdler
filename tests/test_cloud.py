401d1eaf20224c983512fcc92931400d55abd547
Amr Abouelleil <amr@broadinstitute.org>
2018-02-13 08:51:08 -0500

    cloud storage tests

--- tests/test_cloud.py
+++ tests/test_cloud.py
@@ -8,16 +8,22 @@ class CloudUnitTests(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         self.compute = gc.build('compute', 'v1')
-        self.bucket = "cil-test-bucket"
+        self.bucket = "widdler_test_bucket"
         self.project = "broad-cil-devel"
         self.instance_name = "widdler-cloud-test"
         self.zone = "us-central1"
-        self.storage_client = storage.Client.from_service_account_json(c.service_account_json)
 
-    def test_list_instance(self):
-        buckets = list(self.storage_client.list_buckets())
-        print(buckets)
-        self.assertEqual(True, False)
+    # def test_list_instance(self):
+    #
+    #     print(buckets)
+    #     self.assertEqual(True, False)
+
+    def test_create_bucket(self):
+        storage_client = storage.Client()
+        bucket = storage_client.create_bucket(self.bucket)
+        print('Bucket {} created.'.format(bucket.name))
+        self.assertEqual(str(bucket.name), self.bucket)
+
 
     @classmethod
     def tearDownClass(self):
